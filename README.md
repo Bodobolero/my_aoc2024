47318967247855c931fae927de410c4061e4108d
Peter Bendel <peter@familie-bendel.de>
2024-12-01 12:05:27 +0100

    day01

--- README.md
+++ README.md
@@ -13,3 +13,29 @@ cargo run --bin day01
 # run benchmark for a day
 cargo bench day01
 ```
+
+## Benchmarks
+
+| day  |  part1 |  part2 |
+| :----|-------:|-------:|
+| day01| 85 µs | 124 µs |
+
+
+## Benchmark details
+
+Macbook Pro 2019
+2.8 Ghz Intel Core i7
+
+```
+day01_part1             time:   [83.500 µs 84.844 µs 87.276 µs]
+                        change: [-24.038% -23.400% -22.658%] (p = 0.00 < 0.05)
+                        Performance has improved.
+Found 3 outliers among 100 measurements (3.00%)
+  1 (1.00%) low mild
+  1 (1.00%) high mild
+  1 (1.00%) high severe
+
+day01_part2             time:   [123.81 µs 124.20 µs 124.61 µs]
+                        change: [-26.136% -25.838% -25.567%] (p = 0.00 < 0.05)
+                        Performance has improved.
+```
--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,13 +1,38 @@
+use std::collections::HashMap;
 
-#[allow(dead_code)] // not yet used in template
 const INPUT: &str = include_str!("../../inputs/input01.txt");
 
-pub fn part1() -> u32 {
-    42
+pub fn part1() -> i64 {
+    let (mut a, mut b): (Vec<_>, Vec<_>) = INPUT
+        .lines()
+        .map(|l| {
+            let mut split = l.split_whitespace();
+            (split.next().unwrap(), split.next().unwrap())
+        })
+        .map(|(x, y)| (x.parse::<i64>().unwrap(), y.parse::<i64>().unwrap()))
+        .unzip();
+    a.sort_unstable();
+    b.sort_unstable();
+    a.iter().zip(b.iter()).map(|(a, b)| (a - b).abs()).sum()
 }
 
-pub fn part2() -> u32 {
-    42
+pub fn part2() -> i64 {
+    let (a, b): (Vec<_>, Vec<_>) = INPUT
+        .lines()
+        .map(|l| {
+            let mut split = l.split_whitespace();
+            (split.next().unwrap(), split.next().unwrap())
+        })
+        .map(|(x, y)| (x.parse::<i64>().unwrap(), y.parse::<i64>().unwrap()))
+        .unzip();
+    let mut counts = HashMap::<i64, i64>::new();
+    for item in b {
+        let count = counts.entry(item).or_insert(0);
+        *count += 1;
+    }
+    a.iter()
+        .map(|val| *counts.entry(*val).or_default() * val)
+        .sum()
 }
 
 #[cfg(test)]
@@ -16,12 +41,11 @@ mod tests {
 
     #[test]
     fn part1_test() {
-        assert_eq!(part1(), 0);
+        assert_eq!(part1(), 3246517);
     }
-    
+
     #[test]
     fn part2_test() {
-        assert_eq!(part2(), 0);
+        assert_eq!(part2(), 29379307);
     }
 }
-
